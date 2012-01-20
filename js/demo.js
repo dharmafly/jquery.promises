caf57df3d87d8c71cec84cf48e714589a9d4ef26
Aaron Acerboni <a.acerboni@gmail.com>
2012-01-20 16:42:52 +0000

    added ace editor for code examples

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,34 @@
-jQuery('a.btn.run-code')
+// Run through dom creating ace editors out of div.code-example
+
+jQuery('.row div.code-example').each(function(i, dom){
+	createAceEditor(dom);
+});
+
+// Setup code evaluation for run buttons
+jQuery('a.btn.')
     .click(function(){
-        var codeElem = jQuery(this).prev("pre.example"),
-            code = codeElem.text();
+        var editor = jQuery(this).prev().data("editor"),
+            code = editor.getSession().getValue();
 
         jQuery.globalEval(code);
     });
+
+
+function createAceEditor(dom){
+	jQuery(dom).css({
+		"position" : "relative",
+		"width"    : "500px",
+		"height"   : "200px"
+	});
+
+	var editor = ace.edit(dom),
+		session = editor.getSession(),
+		jsMode = require("ace/mode/javascript").Mode;
+
+	session.setTabSize(4);
+	session.setUseSoftTabs(true);
+	session.setMode(new jsMode());
+
+	// Set up accessible references for the code evaluation buttons
+	jQuery(dom).data("editor", editor);
+}
