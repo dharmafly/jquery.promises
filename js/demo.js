1d539c324c69294e4d807c73db9b5a28d092dc1c
Aaron Acerboni <a.acerboni@gmail.com>
2012-01-27 10:44:21 +0000

    changed to soft tabs

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 // Run through dom creating ace editors out of div.code-example
 
 jQuery('.row div.code-example').each(function(i, dom){
-	createAceEditor(dom);
+    createAceEditor(dom);
 });
 
 // Setup code evaluation for run buttons
@@ -16,21 +16,21 @@ jQuery('button.btn.run-code')
 
 function createAceEditor(dom){
 
-	var elem = jQuery(dom);
+    var elem = jQuery(dom);
 
-	elem.css({
-		position: "relative",
-		height: 200,
-		width: 460
-	});
+    elem.css({
+        position: "relative",
+        height: 200,
+        width: 460
+    });
 
-	var editor = ace.edit(dom),
-		session = editor.getSession(),
-		jsMode = require("ace/mode/javascript").Mode;
+    var editor = ace.edit(dom),
+        session = editor.getSession(),
+        jsMode = require("ace/mode/javascript").Mode;
 
-	session.setTabSize(4);
-	session.setUseSoftTabs(true);
-	session.setMode(new jsMode());
+    session.setTabSize(4);
+    session.setUseSoftTabs(true);
+    session.setMode(new jsMode());
 
-	elem.data("editor", editor);
+    elem.data("editor", editor);
 }
