350c31d7864bad08183460285f455e3c8763f3d5
Aron Carroll <self@aroncarroll.com>
2012-03-29 13:45:38 +0100

    Implemented Ace editor in all text areas

--- javascript/demo.js
+++ javascript/demo.js
@@ -10,22 +10,35 @@ function createAceEditor(dom){
 
     editor = ace.edit(dom);
     session = editor.getSession();
-    jsMode = require("ace/mode/javascript").Mode;
-        
-    editor.setTheme("ace/theme/monokai");
-	editor.setHighlightActiveLine(false);
-	editor.setShowPrintMargin(false);
-	
-    session.setTabSize(4);
-    session.setUseSoftTabs(true);
-    session.setMode(new jsMode());
+    JavaScriptMode = require("ace/mode/javascript").Mode;
+    editor.setTheme("ace/theme/clouds");
+    editor.setHighlightActiveLine(false);
+    editor.setShowPrintMargin(false);
+    editor.renderer.setShowGutter(false);
 
+    session.setTabSize(2);
+    session.setUseSoftTabs(true);
+    session.setMode(new JavaScriptMode());
+
+    editor.getSession().on('change',(function onChange(event) {
+        var height = editor.getSession().getDocument().getLength() *
+                     editor.renderer.lineHeight +
+                     editor.renderer.scrollBar.getWidth() * 1.2;
+
+        elem.height(height);
+        elem.find(":first-child").height(elem.height());
+        editor.renderer.onResize(true);
+
+        return onChange;
+    })());
+
+    editor.selection.moveCursorFileEnd();
     elem.data("editor", editor);
 }
 
 // For each code block, create an ACE code editor
-jQuery('div.code-example').each(function(i, dom){
-    createAceEditor(dom);
+jQuery('pre code').each(function(){
+    createAceEditor(this);
 });
 
 // Attach handlers to "Run" buttons
